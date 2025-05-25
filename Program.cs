e0a20258242d6cc8f349ccda01ec38d2292c1b05
Oğuzhan Seslikaya <oguzhanseslikaya7@gmail.com>
2025-05-25 21:44:23 +0300

    added modules

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using ModulesForSubtree.BannersModule.Extensions;
+using ModulesForSubtree.CampaignsModule.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
+
+var connectionString = "Server=localhost;Initial Catalog=ImplementationOfModules;Integrated Security=True;TrustServerCertificate=True;";
+
+builder.Services.AddBannersModule(connectionString);
+builder.Services.AddCampaignsModule(connectionString);
 
 var app = builder.Build();
 
